426738ddbb1ffbc4d9461d16b608e70e918a4559
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2024-03-11 22:38:34 +0300

    fix(harpoon): config

--- .config/nvim/lua/user/plugins/auto-save.lua
+++ .config/nvim/lua/user/plugins/auto-save.lua
@@ -11,7 +11,8 @@ return {
       dim = 0.18, -- dim the color of `message`
       cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
     },
-    trigger_events = { "InsertLeave", "TextChanged" }, -- vim events that trigger auto-save. See :h events
+    trigger_events = { "InsertLeave", "TextChanged" },
+    -- vim events that trigger auto-save. See :h events
     -- function that determines whether to save the current buffer or not
     -- return true: if buffer is ok to be saved
     -- return false: if it's not ok to be saved
@@ -19,9 +20,13 @@ return {
       local fn = vim.fn
       local utils = require "auto-save.utils.data"
 
+      if vim.bo.ft == "harpoon" then return false end
+      vim.cmd "silent! wall"
+
       if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
         return true -- met condition(s), can save
       end
+
       return false -- can't save
     end,
     write_all_buffers = false, -- write all buffers when the current one meets `condition`
@@ -34,5 +39,6 @@ return {
       before_saving = nil, -- ran before doing the actual save
       after_saving = nil, -- ran after doing the actual save
     },
+    autocmd = {},
   },
 }
--- .config/nvim/lua/user/plugins/harpoon.lua
+++ .config/nvim/lua/user/plugins/harpoon.lua
@@ -1,17 +1,53 @@
 local prefix = "<leader><leader>"
+local term_string = vim.fn.exists "$TMUX" == 1 and "tmux" or "term"
+local maps = { n = {} }
+local icon = vim.g.icons_enabled and "󱡀 " or ""
+
+maps.n[prefix] = { desc = icon .. "Harpoon" }
+
+require("astronvim.utils").set_mappings(maps)
+local harpoon = require "harpoon"
+harpoon:setup {}
+
 return {
-  "ThePrimeagen/harpoon",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope.nvim",
-  },
-  cmd = { "Harpoon" },
-  keys = {
-    { prefix, desc = "Harpoon" },
-    { prefix .. "a", function() require("harpoon.mark").add_file() end, desc = "Add file" },
-    { prefix .. "e", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Toggle quick menu" },
-    { prefix .. "p", function() require("harpoon.ui").nav_prev() end, desc = "Goto previous mark" },
-    { prefix .. "n", function() require("harpoon.ui").nav_next() end, desc = "Goto next mark" },
-    { prefix .. "m", "<cmd>Telescope harpoon marks<CR>", desc = "Show marks in Telescope" },
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    keys = {
+      { prefix .. "a", function() harpoon:list():append() end, desc = "Add file" },
+      {
+        prefix .. "e",
+        function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
+        desc = "Toggle quick menu",
+      },
+      {
+        prefix .. "x",
+        function()
+          vim.ui.input({ prompt = "Harpoon mark index: " }, function(input)
+            local num = tonumber(input)
+            if num then harpoon:list():select(num) end
+          end)
+        end,
+        desc = "Goto index of mark",
+      },
+      { prefix .. "p", function() harpoon:list():prev() end, desc = "Goto previous mark" },
+      { prefix .. "n", function() harpoon:list():next() end, desc = "Goto next mark" },
+      { prefix .. "m", "<cmd>Telescope harpoon marks<CR>", desc = "Show marks in Telescope" },
+      {
+        prefix .. "t",
+        function()
+          vim.ui.input({ prompt = term_string .. " window number: " }, function(input)
+            local num = tonumber(input)
+            if num then harpoon:term():select(num) end
+          end)
+        end,
+        desc = "Go to " .. term_string .. " window",
+      },
+    },
+    opts = {},
   },
 }
