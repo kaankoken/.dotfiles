1da74204c734c1beb86c50b0890cf96ff338dbc3
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2024-04-04 13:33:43 +0300

    chore(astronvim): migrate v3 to v4

--- .config/nvim/lua/plugins/auto-save.lua
+++ .config/nvim/lua/plugins/auto-save.lua
@@ -1,7 +1,6 @@
 return {
   "Pocco81/auto-save.nvim",
   event = { "User AstroFile", "InsertEnter" },
-  lazy = false,
   opts = {
     enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
     execution_message = {
@@ -32,12 +31,28 @@ return {
     write_all_buffers = false, -- write all buffers when the current one meets `condition`
     debounce_delay = 135, -- saves the file at most every `debounce_delay` milliseconds
     callbacks = {
-      -- functions to be executed at different intervals
-      enabling = nil, -- ran when enabling auto-save
-      disabling = nil, -- ran when disabling auto-save
-      before_asserting_save = nil, -- ran before checking `condition`
-      before_saving = nil, -- ran before doing the actual save
-      after_saving = nil, -- ran after doing the actual save
+      before_saving = function()
+        -- save global autoformat status
+        vim.g.OLD_AUTOFORMAT = vim.g.autoformat_enabled
+
+        vim.g.autoformat_enabled = false
+        vim.g.OLD_AUTOFORMAT_BUFFERS = {}
+        -- disable all manually enabled buffers
+        for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+          if vim.b[bufnr].autoformat_enabled then
+            table.insert(vim.g.OLD_BUFFER_AUTOFORMATS, bufnr)
+            vim.b[bufnr].autoformat_enabled = false
+          end
+        end
+      end,
+      after_saving = function()
+        -- restore global autoformat status
+        vim.g.autoformat_enabled = vim.g.OLD_AUTOFORMAT
+        -- reenable all manually enabled buffers
+        for _, bufnr in ipairs(vim.g.OLD_AUTOFORMAT_BUFFERS or {}) do
+          vim.b[bufnr].autoformat_enabled = true
+        end
+      end,
     },
     autocmd = {},
   },
