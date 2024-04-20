ffa36ba493fcca78546d14443ced68e149f856c0
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2024-04-20 16:46:33 +0300

    feat(presence): add presence plugin

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -11,6 +11,7 @@ return {
   { import = "astrocommunity.editing-support.comment-box-nvim" },
   { import = "astrocommunity.editing-support.telescope-undo-nvim" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.media.presence-nvim"},
   { import = "astrocommunity.motion.harpoon" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.lua" },
