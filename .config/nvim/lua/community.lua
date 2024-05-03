d3f1e9555b5b4cfda15dbbacd87305f6d8f87ed8
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2024-05-03 13:55:14 +0300

    feat(gc): stop unused lsp

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -13,6 +13,7 @@ return {
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.media.presence-nvim" },
   { import = "astrocommunity.motion.harpoon" },
+  { import = "astrocommunity.lsp.garbage-day-nvim"},
   { import = "astrocommunity.pack.kotlin" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.lua" },
