66cc23eaab22db0e0f8d9f167d373fa6db08bda4
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2024-08-26 00:30:50 +0300

    feat: add python

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -25,6 +25,7 @@ return {
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.ruby" },
   { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.python-ruff"},
   { import = "astrocommunity.recipes.disable-tabline" },
   { import = "astrocommunity.workflow.hardtime-nvim" },
 }
